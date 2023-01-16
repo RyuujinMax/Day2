0498760d3f75d66738cb485fe21ed62ec96de6d7
Giovani Fogarolli <fogarolligiovani@gmail.com>
2023-01-16 17:57:05 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,42 @@
+// Receber os dados
+
+var nomecompleto, idade, linguagem;
+
+function registro(){
+    nomecompleto = document.getElementById("nome_completo").value;
+    idade = document.getElementById("idade").value;
+    linguagem = document.getElementById("linguagem").value;
+}
+
+const enviou = document.getElementsByClassName(".enviar").checked;
+
+function logSubmit(event) {
+    registro();
+    document.querySelector("#form").classList.add('hide');
+    document.querySelector("#fieldset").classList.remove('hide');
+    document.querySelector("#div").classList.add('segundapagina');
+    log.textContent = "Olá " + nomecompleto + ", você tem " + idade + " anos e já está aprendendo " + linguagem + "!";
+    pergunta.textContent = "Você gosta de estudar " + linguagem + "?";
+    event.preventDefault();
+  }
+  
+const form = document.getElementById('form');
+const log = document.getElementById('log');
+const pergunta = document.getElementById('pergunta');
+form.addEventListener('submit', logSubmit);
+
+function textoRadio(){
+    var radio1 = document.getElementById('radio-sim');
+    var radio2 = document.getElementById('radio-nao');
+
+    if(radio1.checked){
+        logradio.textContent = "Muito bom! Continue estudando e você terá muito sucesso."
+    }
+    if(radio2.checked){
+        logradio.textContent = "Ahh que pena... Já tentou aprender outras linguagens?"
+    }
+}
+
+const fieldset = document.getElementById('fieldset');
+fieldset.addEventListener('change', textoRadio);
+const logradio = document.getElementById('logradio');
